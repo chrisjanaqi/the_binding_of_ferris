383b803191e00892b633084ca86998eb4e2398df
Chris Janaqi <chris.janaqi@gmail.com>
2020-11-03 20:18:57 +0100

    Switch to ggez + legion

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,51 @@
-mod isaac;
-
-use crate::isaac::Isaac;
-use amethyst::{
-    prelude::*,
-    renderer::{types::DefaultBackend, RenderFlat2D, RenderToWindow, RenderingBundle},
-    utils::application_root_dir,
+use ggez::nalgebra as na;
+use ggez::{
+    event::{self, EventHandler},
+    graphics,
+    timer::{delta, fps},
+    Context, ContextBuilder, GameResult,
 };
+use legion::*;
 
-fn main() -> amethyst::Result<()> {
-    amethyst::start_logger(Default::default());
-    let app_root = application_root_dir()?;
-    let config_dir = app_root.join("config");
-    let asset_dir = app_root.join("assets");
+type Point = na::Point2<f32>;
+// type Vector = na::Vector2<f32>;
 
-    let game_data = GameDataBuilder::default().with_bundle(
-        RenderingBundle::<DefaultBackend>::new()
-            .with_plugin(
-                RenderToWindow::from_config_path(config_dir.join("display.ron"))?
-                    .with_clear([0.00196, 0.23726, 0.21765, 1.0]),
-            )
-            .with_plugin(RenderFlat2D::default()),
-    )?;
-    let mut game = Application::new(asset_dir, Isaac, game_data)?;
-    game.run();
+#[derive(Default)]
+struct Isaac {
+    world: World,
+}
 
-    println!("hello world!");
-    Ok(())
+impl Isaac {
+    pub fn new(ctx: &mut Context) -> Self {
+        Isaac {
+            world: World::default(),
+        }
+    }
+}
+
+impl EventHandler for Isaac {
+    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+        let dt = delta(ctx).as_secs_f64();
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, graphics::BLACK);
+        let fps = graphics::Text::new(format!("{:.0}", fps(ctx)));
+        graphics::draw(ctx, &fps, (Point::new(20.0, 20.0),))?;
+        graphics::present(ctx)
+    }
+}
+
+fn main() {
+    // Make a Context and an EventLoop.
+    let (mut ctx, mut event_loop) = ContextBuilder::new("isaac-tears", "ergo_games")
+        .build()
+        .unwrap();
+
+    let mut game = Isaac::new(&mut ctx);
+    match event::run(&mut ctx, &mut event_loop, &mut game) {
+        Ok(_) => println!("Exited cleanly."),
+        Err(e) => println!("Error occurred: {}", e),
+    }
 }
